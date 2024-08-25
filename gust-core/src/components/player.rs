a406e0d67fed538b717e2d3648e22baac331a64b
Geert Haans <geert.haans@gmail.com>
2024-08-25 16:33:40 +0200

    refactor: player movement

--- gust-core/src/components/player.rs
+++ gust-core/src/components/player.rs
@@ -27,9 +27,9 @@ impl Player {
     }
 
     pub fn init() -> Self {
-        let position = Vect3::from_slice(&[-5.0, 0.0, 0.0]);
-        let direction = Vect3::from_slice(&[1.0, 0.0, 0.0]);
-        let up = Vect3::from_slice(&[0.0, 0.0, 1.0]);
+        let position = Vect3::new(-5.0, 0.0, 0.0);
+        let direction = Vect3::new(1.0, 0.0, 0.0);
+        let up = Vect3::new(0.0, 0.0, 1.0);
         let speed = 0.1;
 
         Player::new(position, direction, up, speed)
@@ -48,16 +48,15 @@ impl Player {
         let delta_x = delta_x * sensitivity;
         let delta_y = delta_y * sensitivity;
 
-        let up = Vect3::from_slice(&[0.0, 0.0, 1.0]);
-
-        let rotation_matrix_side = Mat3::rotation_matrix(&up, -delta_x);
+        let rotation_matrix_side = Mat3::rotation_matrix(&self.up, -delta_x);
 
         let mut new_direction = self.direction.clone();
 
-        let right = self.direction.cross(&up).normalize();
+        let right = self.direction.cross(&self.up).normalize();
 
-        let vertical_angle = self.direction.dot(&up).acos();
+        let vertical_angle = self.direction.dot(&self.up).acos();
 
+        // Clamp the vertical angle
         if vertical_angle - delta_y > Self::MAX_VERTICAL_ANGLE || vertical_angle - delta_y < Self::MIN_VERTICAL_ANGLE {
             new_direction = rotation_matrix_side.clone() * new_direction.normalize();
             self.direction = new_direction;
@@ -74,42 +73,39 @@ impl Player {
     }
 
     fn update_position(&mut self, game_input: &InputHandler) {
-        let direction = self.direction.clone();
-        let mut position = self.position.clone();
+        let look_direction = self.direction.clone();
+        let up = self.up.clone();
 
-        let mut x_y_direction = Vect3::from_slice(&[direction[0], direction[1], 0.0]);
-        x_y_direction.normalize();
+        let horizontal = (look_direction - up * look_direction.dot(&up)).normalize();
+        let right = horizontal.cross(&up).normalize();
 
         let mut cumulative_vector = Vect3::from_slice(&[0.0, 0.0, 0.0]);
 
         if game_input.keyboard_input.is_character_pressed('w') {
-            cumulative_vector = cumulative_vector + x_y_direction.clone();
+            cumulative_vector = cumulative_vector + horizontal.clone();
         }
 
         if game_input.keyboard_input.is_character_pressed('s') {
-            cumulative_vector = cumulative_vector - x_y_direction.clone();
+            cumulative_vector = cumulative_vector - horizontal.clone();
         }
 
         if game_input.keyboard_input.is_character_pressed('a') {
-            let right = Vect3::from_slice(&[x_y_direction[1], -x_y_direction[0], x_y_direction[2]]);
             cumulative_vector = cumulative_vector + right;
         }
 
         if game_input.keyboard_input.is_character_pressed('d') {
-            let right = Vect3::from_slice(&[x_y_direction[1], -x_y_direction[0], x_y_direction[2]]);
             cumulative_vector = cumulative_vector - right;
         }
 
         if game_input.keyboard_input.is_key_pressed(Key::Named(NamedKey::Space)) {
-            cumulative_vector[2] += 1.0;
+            cumulative_vector = cumulative_vector + up;
         }
 
         if game_input.keyboard_input.is_key_pressed(Key::Named(NamedKey::Shift)) {
-            cumulative_vector[2] -= 1.0;
+            cumulative_vector = cumulative_vector - up;
         }
 
         cumulative_vector.normalize();
-        position = position + cumulative_vector * self.speed;
-        self.position = position;
+        self.position = self.position + cumulative_vector * self.speed;
     }
 }
